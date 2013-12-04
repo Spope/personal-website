a3270117da7af32b64b5ff01a9bd23412f4b9629
Thomas Pinaud <pinaudt@gmail.com>
2013-12-04 16:40:20 +0100

    first commit

---
+++ gruntfile.js
@@ -0,0 +1,36 @@
+module.exports = function(grunt) {
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        less: {
+            development: {
+                options: {
+                    //paths: ["../client/public/less"]
+                },
+                files: {
+                    "styles/main.css" : "less/main.less"
+                }
+            }
+        },
+        watch: {
+            less: {
+                files: "less/*.less",
+                tasks: ["less"],
+                options: {
+                    interrupt: true
+                }
+            },
+            livereload: {
+                options: {
+                    livereload: true
+                },
+                files: ["styles/main.css"]
+            }
+        },
+    });
+
+
+    grunt.loadNpmTasks('grunt-contrib-less');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+
+    grunt.registerTask('default', ['less', 'watch']);
+};
