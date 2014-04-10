baad2e22c30ff974c55ef6001145ec18c0c8d778
Thomas Pinaud <pinaudt@gmail.com>
2014-04-10 20:26:51 +0200

    Scroll ratio

--- web/public/js/lib/scroll.js
+++ web/public/js/lib/scroll.js
@@ -10,7 +10,7 @@ var Scroll = {
         window.addEventListener('scroll', function(e) {
             var doc = document.documentElement, body = document.body;
             var top = (doc && doc.scrollTop  || body && body.scrollTop  || 0);
-            document.querySelector('#header-home').style.backgroundPosition = "50% "+top/3+"px";
+            document.querySelector('#header-home').style.backgroundPosition = "50% "+top/2+"px";
         });
     },
 
