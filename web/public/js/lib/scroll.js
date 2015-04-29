0e5c17110570f2d5fa9690fa82f6bbe1c6aad81b
Thomas Pinaud <pinaudt@gmail.com>
2015-04-29 20:38:06 +0200

    Fixed transform method to work on any engine

--- web/public/js/lib/scroll.js
+++ web/public/js/lib/scroll.js
@@ -1,20 +1,21 @@
 var Scroll = {
 
     _ground: null,
+    _transform: null,
 
     init: function() {
         if(!this.checkBrowser()) {
+            this._transform = this.getSupportedTransform();
             this._ground = document.getElementById('header-home-background');
             window.requestAnimationFrame(this.scrollParallax.bind(this));
         }
     },
 
     scrollParallax: function() {
-
         var posY = (document.documentElement.scrollTop) ? document.documentElement.scrollTop : window.pageYOffset;
         posY = posY.toFixed(2);
         if(posY < 550) {
-            this._ground.style.transform = 'translate3d(' + 0 + 'px, ' + (posY / 3) + 'px,0)';
+            this._ground.style[this._transform] = 'translate3d(' + 0 + 'px, ' + (posY / 3) + 'px,0)';
         }
 
         window.requestAnimationFrame(this.scrollParallax.bind(this));
@@ -23,10 +24,9 @@ var Scroll = {
     checkBrowser: function() {
         //|| navigator.userAgent.match(/Android/i)
         //|| navigator.userAgent.match(/iPad/i)
+        //|| navigator.userAgent.match(/iPhone/i)
+        //|| navigator.userAgent.match(/iPod/i)
         if(navigator.userAgent.match(/webOS/i)
-           || navigator.userAgent.match(/iPhone/i)
-           
-           || navigator.userAgent.match(/iPod/i)
            || navigator.userAgent.match(/BlackBerry/i)
            || navigator.userAgent.match(/Windows Phone/i)
         ){
@@ -35,5 +35,16 @@ var Scroll = {
         else {
             return false;
         }
+    },
+
+    getSupportedTransform: function() {
+        var prefixes = 'transform WebkitTransform MozTransform OTransform msTransform'.split(' ');
+        var div = document.createElement('div');
+        for(var i = 0; i < prefixes.length; i++) {
+            if(div && div.style[prefixes[i]] !== undefined) {
+                return prefixes[i];
+            }
+        }
+        return false;
     }
 }
