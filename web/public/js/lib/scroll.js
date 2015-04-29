5a8e9bb4c11e578ab617acdc473a4f5d17664212
Thomas Pinaud <pinaudt@gmail.com>
2015-04-29 20:02:48 +0200

    Parallax is now working with requestanimationframe

--- web/public/js/lib/scroll.js
+++ web/public/js/lib/scroll.js
@@ -1,29 +1,31 @@
 var Scroll = {
 
+    _ground: null,
+
     init: function() {
         if(!this.checkBrowser()) {
-            this.scrollParallax();
+            this._ground = document.getElementById('header-home-background');
+            window.requestAnimationFrame(this.scrollParallax.bind(this));
         }
     },
 
     scrollParallax: function() {
 
-        var ground = document.getElementById('header-home-background');
-        window.onscroll = function() {
+        var posY = (document.documentElement.scrollTop) ? document.documentElement.scrollTop : window.pageYOffset;
+        posY = posY.toFixed(2);
+        if(posY < 550) {
+            this._ground.style.transform = 'translate3d(' + 0 + 'px, ' + (posY / 3) + 'px,0)';
+        }
 
-            var posY = (document.documentElement.scrollTop) ? document.documentElement.scrollTop : window.pageYOffset;
-            if(posY < 550) {
-                //ground.style.backgroundPosition = "50% " + (-posY/2) + "px"; 
-                ground.style.transform = 'translate3d(' + 0 + 'px, ' + (posY / 3) + 'px,0)';
-            }
-        };
+        window.requestAnimationFrame(this.scrollParallax.bind(this));
     },
 
     checkBrowser: function() {
-        if( navigator.userAgent.match(/Android/i)
-           || navigator.userAgent.match(/webOS/i)
+        //|| navigator.userAgent.match(/Android/i)
+        //|| navigator.userAgent.match(/iPad/i)
+        if(navigator.userAgent.match(/webOS/i)
            || navigator.userAgent.match(/iPhone/i)
-           || navigator.userAgent.match(/iPad/i)
+           
            || navigator.userAgent.match(/iPod/i)
            || navigator.userAgent.match(/BlackBerry/i)
            || navigator.userAgent.match(/Windows Phone/i)
